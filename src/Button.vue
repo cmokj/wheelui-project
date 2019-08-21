27ba15cb6ba3dd75351a96aea497df775b159025
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-08-21 14:19:27 +0800

    添加 click 事件

--- src/Button.vue
+++ src/Button.vue
@@ -1,7 +1,9 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon class="loading" name="loading"></g-icon>
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <button class="g-button"
+            :class="{[`icon-${iconPosition}`]:true}"
+            @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -11,6 +13,10 @@
     export default {
         props: {
             icon: {},
+            loading: {
+                type: Boolean,
+                default: false
+            },
             iconPosition: {
                 type: String,
                 default: 'left',
@@ -77,7 +83,7 @@
         }
 
         > .loading {
-            animation: spin 2s infinite;
+            animation: spin 1s infinite linear;
         }
     }
 </style>
