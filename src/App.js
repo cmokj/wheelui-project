22b6afaeed00860a00de5f770042934a7c5954f2
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-08-21 16:33:20 +0800

    使用 chai.expect 进行单元测试

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import  Vue from 'vue';
 import  Button from './Button';
 import Icon from './Icon';
 import ButtonGroup from './Button-group';
+import chai from 'chai';
 
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
@@ -15,3 +16,68 @@ new Vue({
         loading3: false
     }
 })
+
+const expect = chai.expect;
+// 单元测试
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-setting');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    })
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-loading');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('2');
+    vm.$el.remove();
+    vm.$destroy();
+}
